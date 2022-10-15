51e702c77ca681a01275900d8cb9d92b6b7c5903
oreshnyk <55344626+oreshnyk@users.noreply.github.com>
2022-10-15 15:59:58 +0200

    task-4
    Forbes

--- src/components/CryptoHistory/CryptoHistory.jsx
+++ src/components/CryptoHistory/CryptoHistory.jsx
@@ -1,5 +1,27 @@
 import { BaseTable, THead, Th, Tr, Td } from './CryptoHistory.styled';
+import { format } from 'date-fns';
 
-export const CryptoHistory = () => {
-  return <div>CryptoHistory</div>;
+
+export const CryptoHistory = ({items}) => {
+  return <BaseTable>
+  <THead>
+    <tr>
+      <Th>№</Th>
+      <Th>PRICE</Th>
+      <Th>AMOUNT</Th>
+      <Th>DATE</Th>
+    </tr>
+  </THead>
+
+  <tbody>
+    {items.map(({id, price, amount, date}, idx) => {
+      return <Tr key={id}>
+      <Td>{idx+1}</Td>
+      <Td>{price}</Td>
+      <Td>{amount}</Td>
+      <Td>{format(new Date(date), 'Pp', { addSuffix: true })}</Td>
+    </Tr>
+    })}
+  </tbody>
+</BaseTable>
 };
